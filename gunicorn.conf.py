3fa17a10296b5945b5259a68d8099e42145ef4e8
ami-nope <25155892@kiit.ac.in>
2026-02-13 14:46:40 +0530

    Harden PORT parsing in gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,4 +4,8 @@ workers = 1
 worker_class = 'gevent'
 timeout = 120
 graceful_timeout = 30
-bind = '0.0.0.0:' + os.environ.get('PORT', '10000')
+try:
+    port = int(os.environ.get('PORT', '10000'))
+except (TypeError, ValueError):
+    port = 10000
+bind = f'0.0.0.0:{port}'
